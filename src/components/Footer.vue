c1d8db1d2dc270901f09b40aa016926f2456d12f
Magda Balun <magda.balun@gmail.com>
2018-10-11 13:02:13 +0200

    The difrences between references type and primitive type

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,31 @@
 <template>
   <footer>
-      <p>{{copyright}}</p>
+      <p>{{copyright}} {{title}}</p>
   </footer>
 </template>
 
 <script>
-
 export default {
+  props: {
+    title: {
+      type: String
+    }
+  },
   data() {
     return {
-        copyright: 'Copyright 2018 Vue Ninjas'
+      copyright: "Copyright 2018"
     };
   }
 };
 </script>
 
 <style scoped>
-footer{
-    background: #000;
-    padding: 6px;
+footer {
+  background: #000;
+  padding: 6px;
 }
-p{
-    color: #fff;
-    text-align: center;
+p {
+  color: #fff;
+  text-align: center;
 }
 </style>
--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -6,6 +6,7 @@
             <h3 v-show="ninja.show">{{ninja.speciality}}</h3>
           </li>
       </ul>
+      <button v-on:click="deleteNinja">Delete Ninja</button>
   </div>
 </template>
 
@@ -30,9 +31,14 @@ export default {
       //     { name: "Kami", speciality: "Webpack", show: false },
       //     { name: "Yoshi", speciality: "Data Diggin", show: false }
       //   ]
-    };
+    }
+  },
+  methods: {
+    deleteNinja: function(){
+      this.ninjas.pop();
+    }
   }
-};
+}
 </script>
 
 <style scoped>
