6e928637f48b93b5fdf29792a8ea1706c6a82442
Magda Balun <magda.balun@gmail.com>
2018-10-11 12:26:50 +0200

    Create first VueJS app with VueCLI

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+      <p>{{copyright}}</p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+        copyright: 'Copyright 2018 Vue Ninjas'
+    };
+  }
+};
+</script>
+
+<style scoped>
+footer{
+    background: #000;
+    padding: 6px;
+}
+p{
+    color: #fff;
+    text-align: center;
+}
+</style>
